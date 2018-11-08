c8cf4325bdc68129a669d34479e73cf2cf0b435a
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-11-09 00:54:09 +0100

    working

--- app/src/main/java/com/example/adambenyahia/kotlincatchgame/MainActivity.kt
+++ app/src/main/java/com/example/adambenyahia/kotlincatchgame/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.adambenyahia.kotlincatchgame
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    var score : Int 0
+    var score : Int = 0
+    var imageArray = ArrayList<ImageView>()
+    var handler : Handler = Handler()
+    var runnable : Runnable = Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +22,17 @@ class MainActivity : AppCompatActivity() {
 
         score = 0
 
+        imageArray = arrayListOf(imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9)
+
+        hideImages()
+
         object : CountDownTimer(10000, 1000){
             override fun onFinish() {
                 timeText.text = "Time's Out"
+                handler.removeCallbacks(runnable)
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
             }
 
             override fun onTick(p0: Long) {
@@ -26,6 +40,29 @@ class MainActivity : AppCompatActivity() {
             }
 
         }.start()
+    }
+
+    fun hideImages(){
+
+        runnable = object : Runnable {
+            override fun run() {
+
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+
+                val random = Random
+                val index = random.nextInt(8 - 0)
+                imageArray[index].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable, 500)
+            }
+
+        }
+
+        handler.post(runnable)
+
+
     }
 
     fun increaseScore(view: View){
